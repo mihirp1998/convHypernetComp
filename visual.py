95a71713680109324c34a3cb4bc7b24ee50a75f8
Mihir Prabhudesai <mihirp1998.mp@gmail.com>
2019-03-15 17:52:22 +0000

    res

--- visual.py
+++ visual.py
@@ -2,7 +2,7 @@ from tensorboardX import SummaryWriter
 writer = SummaryWriter()
 import numpy as np
 import torch
-a = torch.load('checkpoint100_100vids_wn/hypernet_{}_{:08d}.pth'.format(s, epoch))
+a = torch.load('checkpoint100_100vids_wn/hypernet_{}_{:08d}.pth'.format("iter",0))
 
 # a = np.random.randn(100,16)
 writer.add_embedding(a['context_embeddings.weight'])
